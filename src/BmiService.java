e0de254079702187b9037a8df3a3aeea57cf290a
Kostya <snaaack@.mail.ru>
2022-06-13 12:28:46 +0200

    Adding logic to method BmiService and getting the basic data and displaying the result in the response

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,2 +1,7 @@
 public class BmiService {
+    public int calculate(double weight, double growth) {
+        double BmiExactly = weight / (growth * growth);
+        int Bmi = (int) BmiExactly;
+        return Bmi;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double weightOfTheSubject = 110.7;
+        double growthOfTheSubject = 1.87;
+        int BmiOfTheSubject = service.calculate(weightOfTheSubject,growthOfTheSubject);
+        System.out.println(BmiOfTheSubject);
+    }
 }
